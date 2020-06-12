aa3e6fff94e09425fb44377a7ae666704b9270bc
Nuno Serôdio <nunoserus@gmail.com>
2020-06-12 20:48:49 +0100

    fim de nav_graph.xml

--- app/src/main/java/com/boom/covidchecker/ChecklistFragment.java
+++ app/src/main/java/com/boom/covidchecker/ChecklistFragment.java
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
